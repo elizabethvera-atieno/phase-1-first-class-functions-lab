90ca9c983dd6f7f873198a8741e18d175b09f64b
elizabethvera-atieno <atienolizvera@gmail.com>
2022-04-19 15:26:42 +0300

    complete test

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+function returnFirstTwoDrivers(){
+    const firstTwoDrivers = drivers.slice(0, 2)
+    return firstTwoDrivers
+}
+function returnLastTwoDrivers(){
+    const lastTwoDrivers = drivers.slice(2, 4)
+    return lastTwoDrivers
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier (number){
+    return function fareMultiplier(fare){
+        return number * fare
+    }
+}
+function fareDoubler(fare){
+    return fare*2
+}
+
+function fareTripler(fare){
+    return fare*3
+}
+
